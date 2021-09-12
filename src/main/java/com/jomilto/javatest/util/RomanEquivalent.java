9bb50dd4e46cc02d28e55ba83724e914475b952e
Jose Lobato <joselobatof@gmail.com>
2021-09-12 01:34:45 -0500

    add class to define Roman Equivalents

---
+++ src/main/java/com/jomilto/javatest/util/RomanEquivalent.java
@@ -0,0 +1,11 @@
+package com.jomilto.javatest.util;
+
+public class RomanEquivalent {
+    int arabic;
+    String roman;
+
+    public RomanEquivalent(int arabic, String roman) {
+        this.arabic = arabic;
+        this.roman = roman;
+    }
+}
