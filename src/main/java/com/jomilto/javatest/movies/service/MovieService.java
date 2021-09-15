d42f4b05de91f96edfcd366b2470b7ff57f1f68b
Jose Lobato <joselobatof@gmail.com>
2021-09-14 23:41:04 -0500

    add findMoviesById Method to MovieService

--- src/main/java/com/jomilto/javatest/movies/service/MovieService.java
+++ src/main/java/com/jomilto/javatest/movies/service/MovieService.java
@@ -1,4 +1,25 @@
 package com.jomilto.javatest.movies.service;
 
+import com.jomilto.javatest.movies.data.MovieRepository;
+import com.jomilto.javatest.movies.model.Genre;
+import com.jomilto.javatest.movies.model.Movie;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 public class MovieService {
+
+    private MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Collection<Movie> findMoviesByGenre(Genre genre) {
+        Collection<Movie> allMovies = movieRepository.findAll()
+                .stream().filter(movie -> movie.getGenre() == genre)
+                .collect(Collectors.toList());
+
+        return allMovies;
+    }
 }
