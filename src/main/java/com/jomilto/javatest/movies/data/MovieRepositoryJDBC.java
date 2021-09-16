de75916dcaa0647dadc6c628158725124c30ffdb
Jose Lobato <joselobatof@gmail.com>
2021-09-15 20:18:19 -0500

    add test to find by id and insert

--- src/main/java/com/jomilto/javatest/movies/data/MovieRepositoryJDBC.java
+++ src/main/java/com/jomilto/javatest/movies/data/MovieRepositoryJDBC.java
@@ -17,7 +17,8 @@ public class MovieRepositoryJDBC implements MovieRepository {
 
     @Override
     public Movie findByID(long id) {
-        return null;
+        Object[] args = { id };
+        return jdbcTemplate.queryForObject("select * from movies where id = ?", args, movieMapper);
     }
 
     @Override
@@ -28,7 +29,11 @@ public class MovieRepositoryJDBC implements MovieRepository {
 
     @Override
     public void saveOrUpdate(Movie movie) {
-
+        jdbcTemplate.update("insert into movies (name,minutes,genre) values (?,?,?)",
+                    movie.getName(),
+                    movie.getMinutes(),
+                    movie.getGenre().toString()
+                );
     }
 
     private static RowMapper<Movie> movieMapper = (rs, i) ->
--- src/test/java/com/jomilto/javatest/movies/data/MovieRepositoryIntegrationTest.java
+++ src/test/java/com/jomilto/javatest/movies/data/MovieRepositoryIntegrationTest.java
@@ -2,6 +2,8 @@ package com.jomilto.javatest.movies.data;
 
 import com.jomilto.javatest.movies.model.Genre;
 import com.jomilto.javatest.movies.model.Movie;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,6 +13,7 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 import javax.sql.DataSource;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -18,12 +21,21 @@ import static org.junit.Assert.*;
 
 public class MovieRepositoryIntegrationTest {
 
-    @Test
-    public void load_all_movies() throws SQLException {
-        DataSource dataSource = new DriverManagerDataSource("jdbc:h2:mem:text;MODE=MYSQL","sa","sa");
+    private MovieRepositoryJDBC movieRepository;
+    private  DataSource dataSource;
+
+    @Before
+    public void setUp() throws Exception {
+
+        dataSource = new DriverManagerDataSource("jdbc:h2:mem:text;MODE=MYSQL","sa","sa");
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        MovieRepositoryJDBC movieRepository = new MovieRepositoryJDBC(jdbcTemplate);
+        movieRepository = new MovieRepositoryJDBC(jdbcTemplate);
+    }
+
+    @Test
+    public void load_all_movies() {
+
         Collection<Movie> movies = movieRepository.findAll();
 
         assertEquals(Arrays.asList(
@@ -31,5 +43,33 @@ public class MovieRepositoryIntegrationTest {
                 new Movie(2, "Memento", 113, Genre.THRILLER),
                 new Movie(3, "Matrix", 136, Genre.ACTION)
         ),movies);
+    }
+
+    @Test
+    public void load_movie_by_id() {
+        Movie movie = movieRepository.findByID(2);
+
+        assertEquals(
+                new Movie(2, "Memento", 113, Genre.THRILLER)
+        , movie);
+    }
+
+    @Test
+    public void insert_a_movie() {
+        Movie movie = new Movie("Son como niños",122,Genre.COMEDY);
+        movieRepository.saveOrUpdate(movie);
+
+        Movie movieFound = movieRepository.findByID(4);
+
+        assertEquals(
+                new Movie(4, "Son como niños",122,Genre.COMEDY),
+                movieFound
+        );
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        final Statement s = dataSource.getConnection().createStatement();
+        s.execute("drop all objects delete files");
     }
 }
